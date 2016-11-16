944673b6c3c65be7b410e8a96cda588f024e7044
Matt Black <matt.black@weather.com>
2016-11-15 22:13:29 -0500

    Update readme

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Checkout this repo, install dependencies, then start the gulp process with the f
 	> npm install
 	> npm start
 ```
+
+*Note:* You'll need to provide your own Youtube Data API key for this to work. You can create an API Key by going to (https://console.developers.google.com).
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyCNlWXut26O_kOAhG5FnTS4L0YD1tce7AI';
+const API_KEY = '';
 
 class App extends Component {
   constructor(props) {
